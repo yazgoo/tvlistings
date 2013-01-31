ecd0ffd3cbfd816e1efbf6636269ab6cc41b09d1
Your Name <you@example.com>
2013-01-31 18:02:24 +0100

    adding github image

--- webtvlistings.rb
+++ webtvlistings.rb
@@ -25,6 +25,11 @@ module WebTVListings::Views
             head do
                 title { "TV Listings" }
             end
+	    a :href => "http://github.com/yazgoo/tvlistings" do
+		    img :style => "position: absolute; top: 0; right: 0; border: 0;",
+		    :src => "https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png",
+		    :alt => "Fork me on GitHub"
+	    end
             h1 "TV Listings"
             body { self << yield }
         end
